dd23a159f0de501c5b0ecf95e850e05f50448687
zemownkee <zemownkee@gmail.com>
2022-10-21 17:12:51 -0400

    Start exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+//validate input
+if(firstNum < 0 || lastNum <0 ||)
 
+//create addition array
+const addArray = [firstNum, lastNum];
+let sum = 0;
+//loop through numbers between first and last to complete array
+for(i = firstNum; i < lastNum; i++) {
+    addArray.push(i);
+}
+
+//sum all numbers in array
+for(const number in addArray) {
+    sum += parseInt(number);
+}
+console.log(sum);
+return sum;
 };
 
 // Do not edit below this line
