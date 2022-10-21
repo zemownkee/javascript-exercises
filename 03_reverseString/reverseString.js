f98f3a27e6723d6548591e156592439dda647080
zemownkee <zemownkee@gmail.com>
2022-10-21 14:47:22 -0400

    Complete exercise three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    
+    let stringArray = input.split('');
+    let reverseArray = stringArray.reverse();
+    let newString = reverseArray.join('');
+    return newString;
 };
 
 // Do not edit below this line
