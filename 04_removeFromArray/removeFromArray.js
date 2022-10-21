f541dfabf897302cbd6d85a9c0d3d7d9858275ae
zemownkee <zemownkee@gmail.com>
2022-10-21 16:27:33 -0400

    Complete exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(...theArgs) {
+   
+    const initialArray = theArgs[0];
+    const removalArgs = theArgs.slice(1);
+    const newArray = [];
+    
+    for(const item of initialArray) {
+        if (removalArgs[removalArgs.indexOf(item)] === item) {
+            continue;
+        } else {
+            newArray.push(item);
+        }
+    }
+    return newArray;
+    };
 // Do not edit below this line
 module.exports = removeFromArray;
