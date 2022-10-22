850d0c73de4476f182034c71ab4b11234ff63904
zemownkee <zemownkee@gmail.com>
2022-10-21 23:32:51 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degFar) {
+  let degCel = (degFar - 32) * (5/9);
+  return parseFloat(degCel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(degCel) {
+  let degFar = (degCel * (9/5)) + 32;
+  return parseFloat(degFar.toFixed(1));
 };
 
 // Do not edit below this line
