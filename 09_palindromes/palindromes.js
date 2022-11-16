36b27b655433d30c2a2fdbecfd56d49c47ff7227
zemownkee <zemownkee@gmail.com>
2022-11-16 16:57:09 -0500

    Complete palindromes, eventually

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
 
-};
+const test = 'this shouldnot 2435 return true';
+const palindromes = function (string) {
+//clean string for comparison
+const testString = string
+                    .toLowerCase()
+                    .replace(/[.,\/#!$%\^&\*;@:{}=\-_`~()]/g, "")
+                    .replace(/ /g, '');
 
+return( 
+    testString  
+            .split('')
+            .reverse()
+            .join('') == testString);
+
+}
 // Do not edit below this line
 module.exports = palindromes;
