c91384356867cd9f59fc8eab69f924941e9f715c
zemownkee <zemownkee@gmail.com>
2022-11-16 11:48:51 -0500

    Complete first half of calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
-	
+const add = function(...args) {
+	let total = 0;
+  if (args === undefined) return 0;
+
+  for(let arg of args) {
+    total += arg;
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arrayArg) {
+  const output = arrayArg.reduce((total, item) => total + item, 0)
+  return output;
 };
 
 const multiply = function() {
