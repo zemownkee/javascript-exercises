4932d73c0c0df4402b75178ea3251e5c392f5d27
zemownkee <zemownkee@gmail.com>
2022-11-17 18:45:49 -0500

    Complete exercise 10'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(count) {
+    let a = 0;
+    let b = 1;
+    if (count < 0) return "OOPS";
+    if (count < 2) return 1;
+    for(let i = 1; i < count; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+return b;
+}
 // Do not edit below this line
 module.exports = fibonacci;
